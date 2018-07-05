1538b4b77846fa2941cd3ed04283863171783421
bindug108 <bindug@hotmail.co.uk>
2018-07-05 07:46:06 +0100

    Giving Tooltip advice & Images

---
+++ HTMLApp/wwwroot/js/advice.js
@@ -0,0 +1,6 @@
+﻿function showkey(e) {
+    var obj = (navigator.appName === "Microsoft Internet Explorer") ? event : e;
+    if (obj.keyCode === 89 || obj.keyCode === 121)
+        alert("Y pressed. Thank you.");
+}
+document.onkeydown = showkey
